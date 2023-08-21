c311d0748e19401d2b4fbec4e7b1b455441c3c1a
kendall529 <kendall.smith529@gmail.com>
2023-08-20 22:19:25 -0500

    starter files and models complete

---
+++ models/Thought.js
@@ -0,0 +1,71 @@
+const { Schema, model, Types } = require('mongoose');
+
+const thoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: 'Thought is required',
+            minlength: 1,
+            maxlength: 280,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            // Use a getter method to format the timestamp on query
+            get: (timestamp) => dateFormat(timestamp),
+        },
+        username: {
+            type: String,
+            Required: true,
+        },
+        reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+const reactionSchema = new Schema(
+    {
+        reactionId: {
+            // Mongoose ObjectId data type
+            type: Schema.Types.ObjectId,
+            // Default value set to new ObjectId
+            default: () => new Types.ObjectId(),
+        },
+        reationBody: {
+            type: String,
+            Required: true,
+            maxlength: 280,
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            //Default set to the current timestamp
+            default: Date.now,
+            //Use getter method to format the timestamp on query
+            get: (timestamp) => dateFormat(timestamp),
+        }
+    },
+    {
+        toJSON: {
+            getters: true,
+        },
+        id: false,
+    }
+);
+
+thoughtSchema.virtual('reactionCount').get(function () {
+    return this.reactions.length;
+});
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
---
+++ models/User.js
@@ -0,0 +1,47 @@
+const { Schema, model } = require('mongoose');
+const thoughtSchema = require('./Thought');
+import { isEmail } from 'validator';
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: 'Username is required',
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: 'Email is required',
+      unique: true,
+      match: [/.+@.+\..+/],
+    },
+    thoughts: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Thought'
+        }
+    ],
+    friends: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+        }
+    ],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+userSchema.virtual('friendCount').get(function () {
+    return this.friends.length;
+});
+
+const User = model('User', userSchema);
+
+module.exports = User;
